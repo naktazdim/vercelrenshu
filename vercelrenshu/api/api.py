84601b53792404459293bbfdd4b0bc74506f6697
nakt <nakt@walkure.net>
2023-03-20 00:50:27 +0900

    test

--- vercelrenshu/api/api.py
+++ vercelrenshu/api/api.py
@@ -2,11 +2,13 @@ from typing import Union
 
 import pandas as pd
 from fastapi import FastAPI
+from fastapi.middleware.gzip import GZipMiddleware
 
 from vercelrenshu.api.logic import estimate, theta_to_hoshi
 from vercelrenshu.api.request_body import RequestLR2, RequestMD5, lr2_to_lamps_df, md5_to_lamps_df
 
 app = FastAPI()
+app.add_middleware(GZipMiddleware)
 
 
 @app.get("/")
@@ -15,20 +17,21 @@ def home():
 
 
 @app.post("/estimate_md5")
-def estimate_md5(request: RequestMD5) -> str:
+def estimate_md5(request: RequestMD5) -> dict:
     lamps_df = md5_to_lamps_df(request)
     return _estimate(lamps_df)
 
 
 @app.post("/estimate_lr2")
-def estimate_lr2(request: RequestLR2) -> str:
+def estimate_lr2(request: RequestLR2) -> dict:
     lamps_df = lr2_to_lamps_df(request)
     return _estimate(lamps_df)
 
 
-def _estimate(lamps_df: pd.DataFrame) -> str:
+def _estimate(lamps_df: pd.DataFrame) -> dict:
     estimation = estimate(lamps_df)
-    hoshi = theta_to_hoshi(estimation.theta)
-    print(estimation.recommendation)
-    print(estimation.reverse_recommendation)
-    return f"★{hoshi:.02f}"
+    return {
+        "hoshi": theta_to_hoshi(estimation.theta),
+        "recommendation": estimation.recommendation.to_dict(orient="records"),
+        "reverse_recommendation": estimation.reverse_recommendation.to_dict(orient="records"),
+    }
